ac6908c7633047049d5211c84d779c40cad8c954
Al <zeng92mail@gmail.com>
2015-10-27 03:54:15 +0800

    initial Matching

---
+++ MatchBox.py
@@ -0,0 +1,12 @@
+from sets import Set
+import Match
+
+#aggregation of Matches for a job
+class MatchBox(object)
+    def __init__(self, job):
+        self.job = job
+        self.matches = set()
+    def addMatch(self, new_match):
+        self.matches.add(new_match)
+    def getMatches(self):
+        return self.matches
