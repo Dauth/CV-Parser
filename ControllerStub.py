c6bf9645646bc5e42eae996ffa86f4768da3fe47
Al <zeng92mail@gmail.com>
2015-11-07 22:17:11 +0800

    editing unit testing for matcher

--- ControllerStub.py
+++ ControllerStub.py
@@ -6,15 +6,22 @@ import JobDescNode
 
 class ControllerStub(object):
 
-	def __init__(self):
-		self.facade = FacadeStub()
-		self.matcher = Matcher(facade)
+    def __init__(self):
+        self.facade = FacadeStub()
+        self.matcher = Matcher(facade)
 
-	def process(self, resume, job):
-		resumeNode = ResumeNode("name", "999", "email@email.com", "0", resume)
-		jobNode = JobDescNode(job)
-		ResumeProcessor.construct(resume)
-		ResumeProcessor.construct(job)
-		self.facade.storeJob(job)
-		self.facade.storeResume(resume)
-		matcher.matchAll(0)
+    def process(self, resume, job):
+        resumeNode = ResumeNode("name", "999", "email@email.com", "0", resume)
+        jobNode = JobDescNode(job)
+        ResumeProcessor.construct(resume)
+        ResumeProcessor.construct(job)
+        self.facade.storeJob(job)
+        self.facade.storeResume(resume)
+        matcher.matchAll(0)
+
+    resume = open('..\\qy\\samepleResume', 'r')
+    job = open('...\\qy\\sampleJob1\\', 'r')
+
+    self.process(resume, job)
+
+        
