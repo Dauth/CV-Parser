9bc118fbced52f5cd8f82c6f99f290c611f3396a
gary <garywjd91@gmail.com>
2015-11-09 10:31:59 +0800

    changed logic calculation in matcher.py

--- Matcher.py
+++ Matcher.py
@@ -3,6 +3,7 @@ from MatchBox import MatchBox
 from ResumeNode import ResumeNode
 from JobDescNode import JobDescNode
 from Facade import Facade
+from InformationNode import InformationNode
 
 class Matcher(object):
     db = None
@@ -10,20 +11,35 @@ class Matcher(object):
     def __init__(self, db):
         self.db = db
 
+
     def match(self, resume, job):
-        s = set()
-        #do matching
-        #keywords = job.getImptKeywords()
-        required_experience = set(job.getExperience())
-        required_education = set(job.getEducation())
-        required_skills = job.getSkills()
-        experience = set(resume.getExperience())
-        education = set(resume.getEducation())
-        skills = resume.getSkills()
-        s.update(required_experience.intersection(experience))
-        s.update(required_education.intersection(education))
-        s.update(required_skills.intersection(skills))
-        new_match = Match(resume, job, s)
+        imptKeywordSet = set(InformationNode.convertStringIntoList(job.getImptKeywords()))
+        print(imptKeywordSet)
+        rskillSet = set(resume.getSkills())
+        jskillSet = set(job.getSkills())
+        resultSkillSet = rskillSet.intersection(jskillSet)
+        resultSkillSet2 = rskillSet.intersection(imptKeywordSet)
+        # print(rskillSet)
+        # print(jskillSet)
+        resultSkill = (len(resultSkillSet) + len(resultSkillSet2))
+        rLangSet = set(resume.getLanguage())
+        jLangSet = set(job.getLanguage())
+        resultLangSet = rLangSet.intersection(jLangSet)
+        resultLangSet2 = rskillSet.intersection(imptKeywordSet)
+        # print(rLangSet)
+        # print(jLangSet)
+        resultLang = (len(resultLangSet) + len(resultLangSet2))
+        rLocationSet = resume.getLocation().getCountry()
+        jLocationSet = job.getLocation().getCountry()
+        resultLocationSet = int(rLocationSet == jLocationSet)
+        print(resultLocationSet)
+
+        # print(resumeNode.getEducation())
+        # print(jobNode.getEducation())
+        eduResult = (self.compareEducationBetweenJobandResume(resume.getEducation(), job.getEducation()))
+        expResult = (self.compareExperienceBetweenJobandResume(resume.getExperience(), job.getExperience(),InformationNode.convertStringIntoList(job.getImptKeywords())))
+        finalResult = eduResult + expResult +resultLocationSet + resultLang + resultSkill
+        new_match = Match(resume, job, finalResult)
         return new_match
 
     def matchAll(self, mode = 0):
@@ -41,7 +57,7 @@ class Matcher(object):
                     box.addMatch(new_match)
                 boxes.add(box)
             self.db.addMatchBoxes(boxes)
-            
+
         #Only new resumes are uploaded
         elif mode == 1:
             resume_list = self.db.getNewResumes()
@@ -55,7 +71,7 @@ class Matcher(object):
                     box.addMatch(new_match)
                 boxes.add(box)
             self.db.storeMatchBoxes(boxes)
-            
+
         #Only new jobs are uploaded
         elif mode == 2:
             resume_list = self.db.getAllResumes()
@@ -72,3 +88,18 @@ class Matcher(object):
 
         else:
             return False
+
+    def compareEducationBetweenJobandResume(self, resumeEdu, jobEdu):
+        for key in jobEdu:
+            eduType = key.split('in')[0].strip()
+            eduArea = key.split('in')[1].strip()
+            for keyN in resumeEdu:
+                if eduArea in keyN and eduType in keyN:
+                    return 1
+        return 0
+
+    def compareExperienceBetweenJobandResume(self, resumeExp, jobExp, imptkeywords):
+        resumeSet =set(i.getWorkPositionOrExp() for i in resumeExp)
+        jobSet =set(i.getWorkPositionOrExp() for i in jobExp)
+        imptkeywordsSet = set(imptkeywords)
+        return len(resumeSet.intersection(jobSet)) + len(resumeSet.intersection(imptkeywordsSet))
