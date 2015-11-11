933bb8797c2dddf217d1bae3ecb7451503611a2e
gary <garywjd91@gmail.com>
2015-11-11 14:20:41 +0800

    minor fix

--- Match.py
+++ Match.py
@@ -2,11 +2,11 @@
 # 1 to 1 match between a resume and a job
 # 1 set of matched words
 class Match(object):
-    def __init__(self, resume, job, count, matchedKeywords):
+    def __init__(self, resume, job, count, matchedKeywords = None):
         self.resume = resume
         self.job = job
         self.count = count
-        self.matchedKeywords = set(matchedKeywords)
+        self.matchedKeywords = matchedKeywords
 
     def getCount(self):
         return self.count
